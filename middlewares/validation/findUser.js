7fb84b652da665b84222a90ef17eface523ac1c5
cami98735264 <cr7montealegre2010@gmail.com>
2024-05-11 17:05:37 -0500

    Finish captcha verification

---
+++ middlewares/validation/findUser.js
@@ -0,0 +1,11 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+// const Usuario = require('../../models/Usuario.js');
+
+
+const checkIfDuplicatedUser = async (req, res, next) => {
+}
+
+
+
+module.exports = checkIfDuplicatedUser;
--- router/authentication/register.routes.js
+++ router/authentication/register.routes.js
@@ -1,18 +1,27 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const parametros = ["documento", "nombres", "primer_apellido", "segundo_apellido", "telefono", "contraseña"];
+const longitudes = [
+    {key: "documento", max: 15, min: 5},
+    {key: "nombres", max: 50, min: 5},
+    {key: "primer_apellido", max: 50, min: 5},
+    {key: "segundo_apellido", max: 50, min: 5},
+    {key: "telefono", max: 10, min: 10},
+    {key: "contraseña", max: 50, min: 8}
+];
+
 const register = (req, res) => {
-    const longitudes = {
-        documento: 10,
-        telefono: 10,
-    };
     console.log(req.body);
-    if(!req.body.contraseña || !req.body.documento || !req.body.nombres || !req.body.primer_apellido || !req.body.segundo_apellido || !req.body.telefono) {
+    if(parametros.some(param => !req.body[param])) {
         return res.status(400).json({
             success: false,
             message: "Faltan campos por llenar",
             errorType: "DATA_MISSING"
         });
     }
-    if(Object.keys(req.body).some(key => req.body[key].length > longitudes[key])) {
-        const filteredErrors = Object.keys(req.body).filter(key => req.body[key].length > longitudes[key]);
+    if(longitudes.find(({key, min, max}) => req.body[key].length < min || req.body[key].length > max)) {
+        const filteredErrors = longitudes.filter(({key, min, max}) => req.body[key].length < min || req.body[key].length > max).map(({key, min, max}) => `${key} debe tener entre ${min} y ${max} caracteres`);
         console.log(filteredErrors);
         return res.status(400).json({
             success: false,
@@ -21,13 +30,28 @@ const register = (req, res) => {
             errorType: "DATA_LENGTH"
         });
     }
-    if(req.body.contraseña.length < 8) {
-        return res.status(400).json({
+    try {
+        const payload = {
+            documento: req.body.documento,
+            nombres: req.body.nombres,
+            primer_apellido: req.body.primer_apellido,
+            segundo_apellido: req.body.segundo_apellido,
+            telefono: req.body.telefono,
+            contraseña: bcrypt.hashSync(req.body.contraseña, 10),
+            acepta_cookies: req.body.cookies_checkbox
+        }
+        // Falta crear la tabla usuarios en la base de datos
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"});
+        res.cookie("authorization", token, {httpOnly: true, secure: true, maxAge: 3600000, sameSite: "strict"});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
             success: false,
-            message: "La contraseña debe tener al menos 8 caracteres",
-            errorType: "DATA_LENGTH",
+            message: "Error interno del servidor",
+            errorType: "INTERNAL_ERROR"
         });
     }
+
 }
 
 
